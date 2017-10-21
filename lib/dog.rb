dbd04a4e64de505ce8c0a0f01d31aea508b34a6e
Lisa Lee <snickers495@gmail.com>
2017-10-21 23:01:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,10 +9,11 @@ class Dog
   end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
         CREATE TABLE IF NOT EXISTS dogs (
           id INTEGER PRIMARY KEY, name TEXT, breed TEXT
-        )
+        ) SQL
+    DB[:conn].execute(sql)
   end
 
 end
