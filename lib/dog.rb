6b349489bd9eb6d707518527a10845ee29ed4142
Lisa Lee <snickers495@gmail.com>
2017-10-21 23:30:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,7 @@ class Dog
   end
 
   def self.find_or_create_by(hash)
-    dog = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND breed = ?", hash)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", hash)
     if !dog.empty?
       dog = self.new_from_db(dog)
     else
